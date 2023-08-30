155e6d6e352fdc11e6ef221efd23348c3b974fc1
facundo-reinoso <facundoreinoso5@gmail.com>
2023-08-29 23:45:28 -0300

    agrego estilos: index, registrotribunal, mostrarestadisticas

---
+++ js/mostrarestadisticas.js
@@ -0,0 +1,7 @@
+$(function () {
+    $("#fecha_min").datepicker(); // Inicializar el selector de fecha
+});
+
+$(function () {
+    $("#fecha_max").datepicker(); // Inicializar el selector de fecha
+});
