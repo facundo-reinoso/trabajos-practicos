89cf2686417f1cc66756742fc234c788a0eace99
Emmanuel Castro <43184467+emmanuelcastro@users.noreply.github.com>
2023-09-01 21:49:51 -0300

    Agrego estilo a las vistas: principalcstf, registroalumnos, registrointegrantescstf (#5)

---
+++ js/registroalumnos.js
@@ -0,0 +1,7 @@
+$(function () {
+    $("#fecha_baja").datepicker(); // Inicializar el selector de fecha
+});
+
+$(function () {
+    $("#fecha_alta").datepicker(); // Inicializar el selector de fecha
+});
